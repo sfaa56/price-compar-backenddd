13c3e20bc6c2301ae0bb238b926215566331de03
sfaa5 <ym6772003@gmail.com>
2025-10-04 17:47:41 +0300

    fix

--- routes/compare.js
+++ routes/compare.js
@@ -65,12 +65,17 @@ const context = await browser.newContext({
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 })
+
+await context.addInitScript(() => {
+  Object.defineProperty(navigator, 'webdriver', { get: () => false });
+});
+
      const page = await context.newPage();
 
 
 
       const url = `https://www.noon.com/egypt-en/search/?q=${encodeURIComponent(query)}`;
-      await page.goto(url, { waitUntil: "domcontentloaded", timeout: 90000 });
+      await page.goto(url, { waitUntil: "domcontentloaded", timeout: 120000 });
       await page.waitForSelector('div.ProductDetailsSection_wrapper__yLBrw', { timeout: 60000 });
       const card = (await page.$$("div.ProductDetailsSection_wrapper__yLBrw"))[0];
       const title = await card.$eval("h2", el => el.textContent.trim()).catch(() => null);
